d2d7afb815e05279da3f990e27e9ae54f3f9410c
Alex Moore <AVMoore@student.fullsail.edu>
2026-09-10 17:22:27 -0500

    added changes to the apps theme,color, and typography also cleaned up the home screen a little more

--- AndroidApp/app/src/main/java/com/team01/steamanalyst/screens/HomeScreen.kt
+++ AndroidApp/app/src/main/java/com/team01/steamanalyst/screens/HomeScreen.kt
@@ -2,7 +2,7 @@ package com.team01.steamanalyst.screens
 
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
+
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
